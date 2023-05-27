6dc7f655c25309d74dd8ef56b84f6eee56397d14
reinaldoper <reinaldoper83@gmail.com>
2023-05-26 20:39:56 -0400

    feat: adiciona classe Jersey, Error

--- src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
+++ src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
@@ -1,5 +1,31 @@
 package app.trybe.specialityapp.commons;
 
+import javax.ws.rs.core.Response.Status;
+
 public class ApplicationError {
+
+  private Status status;
+  private String message;
+
+  public ApplicationError(Status status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
 
 }
